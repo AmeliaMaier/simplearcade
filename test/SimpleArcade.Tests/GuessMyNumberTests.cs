84988db12a75737f698e17e47e786b50a8719bfb
Amelia Maier <AmeliaMaier@users.noreply.github.com>
2019-10-17 11:00:12 -0600

    add project referance to test suite

---
+++ test/SimpleArcade.Tests/GuessMyNumberTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace SimpleArcade.Tests
+{
+    public class GuessMyNumberTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            GuessMyNumberGame game = new GuessMyNumberGame();
+
+        }
+    }
+}

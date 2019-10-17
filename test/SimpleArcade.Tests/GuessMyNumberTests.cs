98eaa48775a23cb67de24f63c55a0d8e29712a62
Amelia Maier <AmeliaMaier@users.noreply.github.com>
2019-10-17 10:34:31 -0600

    adding framework for unit tests for guessMyNumber

---
+++ test/SimpleArcade.Tests/GuessMyNumberTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SimpleArcade.Tests
+{
+    public class GuessMyNumberTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

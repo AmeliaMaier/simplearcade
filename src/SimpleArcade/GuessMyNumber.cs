0ca82c4d732d85b82ee7ee5a4b0032acd7de6b9d
AmeliaMaier <amelia.maier.jikili@gmail.com>
2019-11-12 17:27:48 -0700

    move random work into util from guess my number

--- src/SimpleArcade/GuessMyNumber.cs
+++ src/SimpleArcade/GuessMyNumber.cs
@@ -91,64 +91,12 @@ namespace SimpleArcade
 
         public int getCorrectNumber()
         {
-            int lowerBound = getLowerBound();
-            int upperBound = getUpperBound(lowerBound);
-            return getRandomNumber(lowerBound, upperBound);
+            RandomUtility rand = new RandomUtility();
+            int lowerBound = rand.getLowerBound();
+            int upperBound = rand.getUpperBound(lowerBound);
+            return rand.getRandomNumber(lowerBound, upperBound);
         }
 
-        public int getLowerBound()
-        {
-            bool notInt = true;
-            int lowerBound = 0;
-            string userInput;
-            while (notInt)
-            {
-                Console.WriteLine("Enter your lower bound: ");
-                userInput = Console.ReadLine();
-                if(int.TryParse(userInput, out lowerBound))
-                {
-                    notInt = false;
-                    lowerBound = int.Parse(userInput);
-                }else
-                {
-                    Console.WriteLine("That number is not recognized. Please try again.");
-                }
-            }
-            return lowerBound;
-        }
-
-        public int getUpperBound(int lowerBound)
-        {
-            bool notInt = true;
-            int upperBound = 0;
-            string userInput;
-            while (notInt)
-            {
-                Console.WriteLine("Enter your upper bound: ");
-                userInput = Console.ReadLine();
-                if(int.TryParse(userInput, out lowerBound))
-                {
-                    notInt = false;
-                    upperBound = int.Parse(userInput);
-
-                    if(upperBound <= lowerBound)
-                    {
-                        notInt = true;
-                        Console.WriteLine("Your upper bound is not greater than your lower bound. Please try again.");
-                    }
-                }else
-                {
-                    Console.WriteLine("That number is not recognized. Please try again.");
-                }
-            }
-            return upperBound;
-        }
-
-        public int getRandomNumber(int lowerBound, int upperBound)
-        {
-            Random rnd = new Random();
-            return rnd.Next(lowerBound, upperBound + 1);
-        }
 
     }
 }

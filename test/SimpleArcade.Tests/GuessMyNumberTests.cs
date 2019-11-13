4f4f7ea9cf7102159b7dd6b1c9f4a18a39c5b3d3
AmeliaMaier <amelia.maier.jikili@gmail.com>
2019-11-12 17:42:09 -0700

    add random util tests, fix naming in guess my number tests

--- test/SimpleArcade.Tests/GuessMyNumberTests.cs
+++ test/SimpleArcade.Tests/GuessMyNumberTests.cs
@@ -8,7 +8,7 @@ namespace SimpleArcade.Tests
         [Fact]
         public void gameInstructions()
         {
-            GuessMyNumberGame game = new GuessMyNumberGame();
+            GuessMyNumber game = new GuessMyNumber();
             string expectedInstructions = "Enter an upper and lower bound. A random integer between the bounds will be created. Each turn, try to guess the number. You will be given hints as you try. Type 'Quite' if you would like to return to the main menu.";
             string actualInstructions = game.getInstructions();
             Assert.Equal(expectedInstructions, actualInstructions);
@@ -17,7 +17,7 @@ namespace SimpleArcade.Tests
         [Fact]
         public void gameInstructionsNotNull()
         {
-            GuessMyNumberGame game = new GuessMyNumberGame();
+            GuessMyNumber game = new GuessMyNumber();
             string actualInstructions = game.getInstructions();
             Assert.NotNull(actualInstructions);
             Assert.NotEmpty(actualInstructions);
@@ -26,7 +26,7 @@ namespace SimpleArcade.Tests
         [Fact]
         public void gameName()
         {
-            GuessMyNumberGame game = new GuessMyNumberGame();
+            GuessMyNumber game = new GuessMyNumber();
             string expectedName = "Guess My Number";
             string actualName = game.getName();
             Assert.Equal(expectedName, actualName);
@@ -35,49 +35,16 @@ namespace SimpleArcade.Tests
         [Fact]
         public void gameNameNotNull()
         {
-            GuessMyNumberGame game = new GuessMyNumberGame();
+            GuessMyNumber game = new GuessMyNumber();
             string actualName = game.getName();
             Assert.NotNull(actualName);
             Assert.NotEmpty(actualName);
         }
 
         [Fact]
-        public void randomNumberInBounds()
-        {
-            GuessMyNumberGame game = new GuessMyNumberGame();
-            int actualRandomNumber = game.getRandomNumber(1, 2);
-            Assert.True(actualRandomNumber == 1 || actualRandomNumber == 2);
-        }
-
-        [Fact]
-        public void randomNumberNegative()
-        {
-            GuessMyNumberGame game = new GuessMyNumberGame();
-            int actualRandomNumber = game.getRandomNumber(-10, -1);
-            Assert.True(actualRandomNumber < 0);
-        }
-
-        [Fact]
-        public void randomNumberPositive()
-        {
-            GuessMyNumberGame game = new GuessMyNumberGame();
-            int actualRandomNumber = game.getRandomNumber(1, 10);
-            Assert.True(actualRandomNumber > 0);
-        }
-
-
-        [Fact]
-        public void randomNumberNotNull()
-        {
-            GuessMyNumberGame game = new GuessMyNumberGame();
-            int actualRandomNumber = game.getRandomNumber(-100, 100);
-            Assert.NotNull(actualRandomNumber);
-        }
-
-        [Fact]
         public void emtpyTest()
         {
-            GuessMyNumberGame game = new GuessMyNumberGame();
+            GuessMyNumber game = new GuessMyNumber();
         }
 
     }
